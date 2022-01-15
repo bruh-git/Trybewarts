a00945955e7b60c0b9efb3b239e337d822a7838d
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-01-15 17:38:30 -0300

    Estilização
    Versão desktop concluída.

--- script.js
+++ script.js
@@ -35,21 +35,21 @@ function fullName() {
   const formName = document.getElementById('input-name');
   const formLastName = document.getElementById('input-lastname');
   const paragraph = document.createElement('p');
-  paragraph.innerHTML = `Nome: ${formName.value} ${formLastName.value}`;
+  paragraph.innerHTML = `<strong>Nome:</strong> ${formName.value} ${formLastName.value}`;
   return paragraph;
 }
 
 function emailAddress() {
   const formEmail = document.getElementById('input-email');
   const paragraph = document.createElement('p');
-  paragraph.innerHTML = `Email: ${formEmail.value}`;
+  paragraph.innerHTML = `<strong>Email:</strong> ${formEmail.value}`;
   return paragraph;
 }
 
 function whichHouse() {
   const formHouse = document.getElementById('house');
   const paragraph = document.createElement('p');
-  paragraph.innerHTML = `Casa: ${formHouse.value}`;
+  paragraph.innerHTML = `<strong>Casa:</strong> ${formHouse.value}`;
   return paragraph;
 }
 
@@ -62,7 +62,7 @@ function whichFamily() {
       string = family.parentElement.innerText;
     }
   });
-  paragraph.innerHTML = `Família: ${string}`;
+  paragraph.innerHTML = `<strong>Família:</strong> ${string}`;
   return paragraph;
 }
 
@@ -70,13 +70,13 @@ function whichTechnologies() {
   const formTechnologies = document.querySelectorAll('input[type="checkbox"][name="technologies"]');
   const paragraph = document.createElement('p');
   let string = '';
-  formTechnologies.forEach((materia) => {
-    if (materia.checked) {
-      string += ` ${materia.parentElement.innerText},`;
+  formTechnologies.forEach((technology) => {
+    if (technology.checked) {
+      string += ` ${technology.parentElement.innerText},`;
     }
   });
   string = string.substring(0, string.length - 1);
-  paragraph.innerHTML = `Matérias:${string}`;
+  paragraph.innerHTML = `<strong>Matérias:</strong>${string}`;
   return paragraph;
 }
 
@@ -89,24 +89,25 @@ function whichRate() {
       string = rate.parentElement.innerText;
     }
   });
-  paragraph.innerHTML = `Avaliação: ${string}`;
+  paragraph.innerHTML = `<strong>Avaliação:</strong> ${string}`;
   return paragraph;
 }
 
 function userComment() {
   const formTextArea = document.getElementById('textarea');
   const paragraph = document.createElement('p');
-  paragraph.innerHTML = `Observações: ${formTextArea.value}`;
+  paragraph.innerHTML = `<strong>Observações:</strong> ${formTextArea.value}`;
   return paragraph;
 }
 
 function printUserData(event) {
   event.preventDefault();
+  const letterDiv = document.getElementById('letter-container');
   const printDiv = document.getElementById('user-information');
   printDiv.innerHTML = '';
   const formInputs = document.getElementById('container-form');
   formInputs.style.display = 'none';
-  printDiv.style.display = 'block';
+  letterDiv.style.display = 'block';
   printDiv.appendChild(fullName());
   printDiv.appendChild(emailAddress());
   printDiv.appendChild(whichHouse());
